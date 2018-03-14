3665c0f4ea9bd6aed2b888b3767ceaedc678338b
tanmaysharma001 <38436868+tanmaysharma001@users.noreply.github.com>
2018-03-14 23:33:35 +0530

    Second code

--- tanmay.cpp
+++ tanmay.cpp
@@ -21,3 +21,19 @@ struct lotteryprocess
 		int ts=0,te;
 		struct lotteryprocess p[n];
 		int i;
+		for(i=0;i<n;i++)	
+		{	
+		printf("Enter the process id for %d process  ",i+1);
+		scanf("%d",&p[i].processid);
+		printf("Enter the Burst Time for %d process  ",(i+1));
+		scanf("%d",&p[i].bursttime);
+		p[i].wait=0;
+		printf("Enter  the ending number of  ticket for %d process ranging from from  %d  to 200  ",i+1,ts);
+		scanf("%d",&te);
+		p[i].firstticket=ts;
+		p[i].lastticket=te;
+		size1=te-ts;
+		p[i].size=size1;
+		p[i].insertticket();
+		ts=te;
+		}
